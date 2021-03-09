10b130fde4d921bf2edfd8ab2e015434ba0ef4cd
tale97 <alessandro.talmi@studio.unibo.it>
2021-03-09 11:10:13 +0100

    updated fib to use tailrec

--- src/exercises/Part3.scala
+++ src/exercises/Part3.scala
@@ -1,11 +1,18 @@
 package exercises
 
+import scala.annotation.tailrec
+
 object Part3 extends App{
+
+  @tailrec def fibStep(nMinusOne: Int, nMinusTwo: Int):Int = nMinusOne match{
+    case 1 => 1
+    case _ => fibStep(nMinusTwo, nMinusTwo - 1)
+  }
 
   def fib(n: Int): Int = n match {
     case n if n < 0 => throw new ArithmeticException("fib does not accept negative values")
     case 0 | 1 => n
-    case _ => fib(n - 1) + fib(n - 2)
+    case _ => fibStep(n-1, n-2)
   }
 
 }
